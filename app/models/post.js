0bcf5f7455d6d4be75d8ab9d41f84966a54fb71f
Steve Tyler <mail@steve-tyler.co.uk>
2014-08-08 21:06:08 -0700

    Created computed property using moment.js and updated template

--- app/models/post.js
+++ app/models/post.js
@@ -1,42 +1,38 @@
 import DS from 'ember-data';
+// import moment from 'bower_components//moment';
 
 var Post = DS.Model.extend({
     // id: DS.attr('text'),  Error: You may not set `id` as an attribute on your model ?
     title: DS.attr('string'),
-    user: DS.belongsTo('user'), 
-    createdDate: DS.attr('date'), 
+    user: DS.belongsTo('user'),
+    createdDate: DS.attr('date'),
     body: DS.attr('string'),
-    // elapsedTime: function() {
-    //     var now = Date.now(),
-    //         createdDate = this.get('createdDate');
-         
-    //     return Date(createdDate);
-        
-    //     }.property('createdDate')
+    displayDate: function() {
+        var date = this.get('createdDate');
+        var now = moment();
+
+          // show mins / hrs if less than a day
+          return moment(date).format("MMM Do");
+        }.property('createdDate')
 });
-
-// App.Requirement = DS.Model.extend({
-
-
-// });
 
 
 Post.reopenClass({
     FIXTURES: [{
         id: 'id1',
         user: 'stevetyler',
-        // createdDate: moment(2014, 8, 5),
+        createdDate: new Date(2014, 8, 5),
         body: 'Ember is great!',
     }, {
         id: 'id2',
         user: 'vivhoford',
-        //createdDate: moment(2014, 8, 6),
+        createdDate: new Date(2014, 8, 6),
         body: 'What\'s Ember?',
 
     }, {
         id: 'id3',
         user: 'rachelblanton',
-        //createdDate: moment(2014, 8, 8),
+        createdDate: new Date(2014, 8, 8),
         body: 'I have no idea what you\'re talking about',
     }]
 });
