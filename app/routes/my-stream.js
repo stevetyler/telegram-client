07fd42e8d80253e4daa50a52bee8d41ea64d3053
Steve Tyler <stevetyler.uk@gmail.com>
2014-08-11 22:41:43 +0100

    Updated my-stream controller to get session object

--- app/routes/my-stream.js
+++ app/routes/my-stream.js
@@ -4,6 +4,12 @@ export
 default Ember.Route.extend({
     model: function() {
         return this.store.find('post'); // returns a promise but is handled by the route
-    }
-
+    },
+    setupController: function(controller, model) {
+		console.log('The session object is accessible from any route:', this.get('session.isAuthenticated'));
+		console.log('We can set the currently authenticated user on the session object, so that we can access it from all the ther routes and controllers.');
+		this.get('session').set('user', model);
+		controller.set('model', model);
+		console.log(this.get('user'));
+	}
 });
