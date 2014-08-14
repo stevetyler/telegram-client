e5889768619f379adac90a05d97a9a29dfb45d0e
Steve Tyler <stevetyler.uk@gmail.com>
2014-08-14 22:09:23 +0100

    Added my-stream publish action

--- app/controllers/my-stream.js
+++ app/controllers/my-stream.js
@@ -4,10 +4,16 @@ export
 default Ember.ArrayController.extend({
 
     actions: {
-	publish: function() {
-		var controller = this;
-		var publish = this.get('post');
-		console.log(publish);
-    	}
-    }
+
+		publish: function() {
+			// var controller = this;
+			var publish = this.get('post');
+			// console.log(publish);
+			var newPost = this.store.createRecord('post', {
+				body: publish
+				});
+			newPost.save();
+			// clear post input field
+		}	
+	}
 });
