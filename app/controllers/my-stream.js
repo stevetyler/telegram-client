77663d7051bd36dc068c3c4fe1ff618b1c439408
Steve Tyler <stevetyler.uk@gmail.com>
2014-08-28 12:28:16 +0100

    Updated publish action to include session user object

--- app/controllers/my-stream.js
+++ app/controllers/my-stream.js
@@ -3,27 +3,20 @@ import Ember from 'ember';
 export
 default Ember.ArrayController.extend({
 
-	postDate : function() {
-		// computed property
-	},
-
-    actions: {
-
+	actions: {
 		publish: function() {
 			// var controller = this;
 			var publish = this.get('newPost');
 			var date = Date.now();
-			// console.log(publish);
 			var newPost = this.store.createRecord('post', {
 				body: publish,
-				name: this.get('session.name')
+				user: this.get('session.user'),
+				date: this.set('date', date) // Sets date for all posts ???
 			});
 			newPost.save();
 			this.set('newPost', null);
-			// this.set('date', date);
-			
 		},
-		// how does this work ?
+		// Action has access to post parameter ie post in controller
 		delete: function(post) {
 			post.deleteRecord();
 			post.save();
