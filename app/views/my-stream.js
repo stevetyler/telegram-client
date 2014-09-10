43d6445753517ea4ec374bb77120a0873effe316
Steve Tyler <stevetyler.uk@gmail.com>
2014-09-10 22:38:45 +0100

    Deleted my-stream view

--- app/views/my-stream.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.View.extend({
-
-	keyUp : function() {
-		var text = this.get('controller.newPost');
-		this.get('controller').send('showCharacters', text.length);
-	}
-});
