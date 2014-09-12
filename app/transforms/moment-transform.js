6507bb4a08ab3a13a0d86a55e234ec2a1af737c7
Steve Tyler <stevetyler.uk@gmail.com>
2014-09-12 18:53:45 +0100

    Created moment transform for Ember-Data

---
+++ app/transforms/moment-transform.js
@@ -0,0 +1,14 @@
+// import DS from 'ember-data'
+
+// MomentTransform = DS.Transform.extend({
+// 	deserialize: function(serialized) {
+// 		return moment(serialized, "YYYY MMM DD");
+// 	}
+    
+//   	serialize: function(deserialized) {
+//   		return deserialized.format("YYYY MMM DD");
+//   	}
+// })
+  
+
+// export default MomentTransform;
