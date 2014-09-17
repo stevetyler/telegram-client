0e8896bf6342873bc6458fc54d65301b33832e05
Steve Tyler <mail@steve-tyler.co.uk>
2014-09-17 14:00:16 +0100

    Renamed my-stream in telegram route and updated notes

--- app/routes/telegram.js
+++ app/routes/telegram.js
@@ -5,7 +5,7 @@ default Ember.Route.extend({
   beforeModel: function() {
     var session = this.get('session');
     if (session.user) {
-      this.transitionTo('my-stream');
+      this.transitionTo('myStream');
     }
     else {
       this.transitionTo('telegram');
