d4c9b6689c7164f0566c226742f259e57dee8819
Steve Tyler <mail@steve-tyler.co.uk>
2014-10-14 22:44:53 +0100

    Check if user is authenticated on all routes

---
+++ app/routes/application.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+    beforeModel: function() {
+        var route = this;
+        var promise = this.store.find('user', {operation: 'authenticated'});
+        return promise.then(function(users) {
+            if (users && users.get('length') > 0) {
+                var user = users.get('firstObject');
+                route.set('session.user', user);
+            }
+            return users;
+        });
+    }
+});
+
