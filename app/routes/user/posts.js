3e4fe7d7209899e119116cb687d10811320c8984
Steve Tyler <mail@steve-tyler.co.uk>
2014-10-23 22:22:48 +0100

    Return posts for specific user

--- app/routes/user/posts.js
+++ app/routes/user/posts.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export
 default Ember.Route.extend({
-    model: function() {
-        return this.store.find('post'); // returns a promise but is handled by the route
-    }
 
+    model: function() {
+
+        var userId = this.modelFor('user').id;  // look up modelFor!
+        console.log('posts route', userId);
+        return this.store.find('post', {ownedBy: userId}); // returns a promise but is handled by the route
+    }
 });
