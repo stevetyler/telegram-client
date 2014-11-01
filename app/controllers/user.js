974351289f0ed122c668e0068ab89caf8f2960d3
Steve Tyler <mail@steve-tyler.co.uk>
2014-11-01 19:08:51 +0000

    Updated actions

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -5,22 +5,26 @@ import Ember from 'ember';
 export
 default Ember.ObjectController.extend({
 
-
-
-
     actions: {
 
         follow: function() {
+            console.log('clicked');
             var controller = this;
-            var IdToFollow = this.get('id');
 
-            // queries go to api/users
-            this.store.find('user', {followUserId: IdToFollow}).then(function() {
+            this.store.find('user', {followUserId: this.get('id')}).then(function() {
+                    controller.set('isFollowed', true);
+                }
+            );
 
-            });
         },
         unfollow: function() {
+            console.log('clicked');
+            var controller = this;
 
+            this.store.find('user', {unFollowUserId: this.get('id')}).then(function() {
+                    controller.set('isFollowed', false);
+                }
+            );
         }
     }
 });
