d051a0f3942a971904326877cf3d8692787e6fa3
Steve Tyler <mail@steve-tyler.co.uk>
2014-11-02 16:33:40 +0000

    Update

--- app/routes/user/followers.js
+++ app/routes/user/followers.js
@@ -6,7 +6,7 @@ default Ember.Route.extend({
     model: function() {
 
         var user = this.modelFor('user');  // look up modelFor!
-        console.log('followers of', user.id);
+        // console.log('followers of', user.id);
 
         return this.store.find('user', {operation: 'followers', userId: user.id});
     }
--- app/routes/user/following.js
+++ app/routes/user/following.js
@@ -5,11 +5,9 @@ default Ember.Route.extend({
 
     model: function() {
 
-        // change to user
-        var userId = this.modelFor('user').id;  // look up modelFor!
-        console.log('Users following', userId);
+        var user = this.modelFor('user');  // look up modelFor!
+        // console.log('Users following', userId);
 
-        return this.store.find('user', {operation: 'following', userId: userId});
-        // rename to userID
+        return this.store.find('user', {operation: 'following', userId: user.id});
     }
 });
