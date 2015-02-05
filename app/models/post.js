41a64f7fb7abcf88bf9b386766329a690abebc8e
Steve Tyler <mail@steve-tyler.co.uk>
2015-02-05 16:23:13 +0000

    Added ownedBy property

--- app/models/post.js
+++ app/models/post.js
@@ -4,10 +4,11 @@ import DS from 'ember-data';
 var Post = DS.Model.extend({
     // id: DS.attr('text'),  Error: You may not set `id` as an attribute on your model ?
     title: DS.attr('string'),
-    user: DS.belongsTo('user'),
+    user: DS.belongsTo('user'), // author
+    ownedBy: DS.belongsTo('user'), // original author
     createdDate: DS.attr('date'), // cannot change name to 'timestamp' etc ???
     body: DS.attr('string'),
-    // cannot move computed property to controller for some reason
+    // cannot move computed property to post controller for some reason
     displayDate: function() {
       var time = moment(this.get('createdDate')).fromNow();
       return time;
