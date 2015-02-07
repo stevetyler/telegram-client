598bf310761338f3e77e3f88a1a29cb569d66c81
Steve Tyler <mail@steve-tyler.co.uk>
2015-02-07 12:59:56 +0000

    Added isCurrentUser property

--- app/controllers/user.js
+++ app/controllers/user.js
@@ -5,26 +5,26 @@ import Ember from 'ember';
 export
 default Ember.ObjectController.extend({
 
-    actions: {
+  isCurrentUser: function() {
+      return this.get('id') === this.get('session.user.id');
+    }.property('id'),
 
-        follow: function() {
-            console.log('clicked');
-            var controller = this;
+  actions: {
+    follow: function() {
+      console.log('clicked');
+      var controller = this;
 
-            this.store.find('user', {followUserId: this.get('id')}).then(function() {
-                    controller.set('isFollowed', true);
-                }
-            );
+      this.store.find('user', {followUserId: this.get('id')}).then(function() {
+        controller.set('isFollowed', true);
+      });
+    },
+    unfollow: function() {
+      console.log('clicked');
+      var controller = this;
 
-        },
-        unfollow: function() {
-            console.log('clicked');
-            var controller = this;
-
-            this.store.find('user', {unFollowUserId: this.get('id')}).then(function() {
-                    controller.set('isFollowed', false);
-                }
-            );
-        }
+    this.store.find('user', {unFollowUserId: this.get('id')}).then(function() {
+      controller.set('isFollowed', false);
+      });
     }
+  }
 });
