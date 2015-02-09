508baa2b16cd5186f16a22c75d48b2a3f58e46a1
Steve Tyler <mail@steve-tyler.co.uk>
2015-02-09 18:06:24 +0000

    Added error argument to promise in order to render login failure message

--- app/controllers/telegram/login.js
+++ app/controllers/telegram/login.js
@@ -5,38 +5,42 @@ import Ember from 'ember';
 export
 default Ember.Controller.extend({ // route has no model (not displaying data) so use Controller not ObjectController
 
-    loginFailed: false,
-    isProcessing: false,
+  loginFailed: false,
+  isProcessing: false,
 
-    actions: {
+  actions: {
 
-        login: function() {
-            var controller = this;
-            var username = this.get('username');
-            var password = this.get('password');
-            this.setProperties({
-                loginFailed: false,
-                isProcessing: true
-            });
+    login: function() {
+      var controller = this;
+      var username = this.get('username');
+      var password = this.get('password');
+      this.setProperties({
+        loginFailed: false,
+        isProcessing: true
+      });
 
-            if (username && password) {
-                password = Ember.$.md5(password + username); // hash username & password
-                // console.log(password);
+      if (username && password) {
+        password = Ember.$.md5(password + username); // hash username & password
 
-                controller.store.find('user', {username: username, password: password, operation: 'login'}).then(function(users) {
+        controller.store.find('user', {username: username, password: password, operation: 'login'}).then(function(users, err) {
 
-                    var user = users.get('firstObject');
-                    controller.set("isProcessing", false);
-                    controller.get('session').set('user', user);
+          if (err) {
+            controller.set('loginFailed', true); // has worked but not aymore?
+            // console.log(controller.get('loginFailed'));
+          }
 
-                    controller.transitionToRoute('myStream');  // why not my-stream ??
-                    controller.set('username', null);
-                    controller.set('password', null);
-                });
-            }
-            else {
-
-            }
-        }
+          var user = users.get('firstObject'); // lookup
+          // controller.set('isProcessing', false);
+          controller.get('session').set('user', user);
+          controller.transitionToRoute('myStream');
+          controller.set('username', null);
+          controller.set('password', null);
+        });
+      }
+      else {
+        controller.set('loginFailed', true);
+        console.log(controller.get('loginFailed'));
+      }
     }
+  }
 });
