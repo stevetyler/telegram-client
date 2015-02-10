e45a7b8325581ce419455383e680dab44174345d
Steve Tyler <mail@steve-tyler.co.uk>
2015-02-10 14:50:14 +0000

    Updated comments

--- app/models/post.js
+++ app/models/post.js
@@ -1,19 +1,18 @@
 import DS from 'ember-data';
-// import moment from 'bower_components//moment';
+// moment is a global variable
 
 var Post = DS.Model.extend({
     // id: DS.attr('text'),  Error: You may not set `id` as an attribute on your model ?
     title: DS.attr('string'),
     user: DS.belongsTo('user'), // author
     ownedBy: DS.belongsTo('user'), // original author
-    createdDate: DS.attr('date'), // cannot change name to 'timestamp' etc ???
+    createdDate: DS.attr('date'),
     body: DS.attr('string'),
     // cannot move computed property to post controller for some reason
     displayDate: function() {
       var time = moment(this.get('createdDate')).fromNow();
       return time;
     }.property('createdDate'),
-
 });
 
 export
