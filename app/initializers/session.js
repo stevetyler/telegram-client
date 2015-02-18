7049c7ba902ccbc15bf69a2825ba110591132fab
Steve Tyler <mail@steve-tyler.co.uk>
2015-02-18 14:31:21 +0000

    Amended comments

--- app/initializers/session.js
+++ app/initializers/session.js
@@ -2,16 +2,15 @@ import Ember from 'ember';
 
 export default {
 	name: 'session',
+  // create session object and inject into all routes and controllers
 	initialize: function(container, app) {
 		var Session = Ember.Object.extend({
 			user: null,
 			isAuthenticated: function() {
 				return this.get('user') !=null;
-			}.property('user')		
+			}.property('user')
 		});
-		// http://emberjs.com/api/classes/Ember.Application.html#method_register
 		app.register('session:main', Session);
-		// http://emberjs.com/api/classes/Ember.Application.html#method_inject
 		app.inject('route', 'session', 'session:main');
 		app.inject('controller', 'session', 'session:main');
 	}
