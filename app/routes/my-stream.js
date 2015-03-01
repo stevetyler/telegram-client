6ca69521e10b387ab52f1fec018c56cde998f07f
Steve Tyler <mail@steve-tyler.co.uk>
2015-03-01 12:09:58 +0000

    Updated route with operation

--- app/routes/my-stream.js
+++ app/routes/my-stream.js
@@ -2,8 +2,16 @@ import Ember from 'ember';
 
 export
 default Ember.Route.extend({
-    model: function() {
-        return this.store.find('post'); // returns a promise but is handled by the route
-    }
 
+  model: function() {
+    var route = this;
+
+    return this.store.filter('post', { operation: 'myStream' }, function (post) {
+      if (post.get('user').id === route.get('session.user.id')) {
+          return true;
+      } else {
+          return post.get('user').get('isFollowed');
+      }
+    });
+  }
 });
