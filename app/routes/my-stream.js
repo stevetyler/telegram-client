563e0d849b9c2541121c5e094c34c7edf77cea22
Steve Tyler <mail@steve-tyler.co.uk>
2015-03-04 18:10:39 +0000

    Added filter method and operation

--- app/routes/my-stream.js
+++ app/routes/my-stream.js
@@ -3,15 +3,29 @@ import Ember from 'ember';
 export
 default Ember.Route.extend({
 
-  model: function() {
-    var route = this;
+  beforeModel: function() {
+    var loggedIn = this.get('session.isAuthenticated');
 
-    return this.store.filter('post', { operation: 'myStream' }, function (post) {
-      if (post.get('user').id === route.get('session.user.id')) {
-          return true;
-      } else {
-          return post.get('user').get('isFollowed');
-      }
+    if (!loggedIn) {
+        this.transitionTo('/');
+    }
+  },
+
+  model: function() {
+    // var route = this;
+
+    // http://emberjs.com/api/data/classes/DS.Store.html#method_filter
+    // Takes a type and filter function, and returns a live RecordArray that remains up to date as new records are loaded into the store or created locally.
+    return this.store.filter('post', { operation: 'myStream' }, function (post){
+
+      //  only returns post for logged in user and not 'isFollowed' property ??
+      // if (post.get('user').id === route.get('session.user.id')) {
+      //     return true;
+      // } else {
+      //     return post.get('user').get('isFollowed');
+      return post;
+
     });
   }
 });
+
