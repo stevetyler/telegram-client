2bbe0b85077b796c5953767edc7c39af483db5bb
Steve Tyler <mail@steve-tyler.co.uk>
2015-03-04 18:11:58 +0000

    Added operation

--- app/routes/user/posts.js
+++ app/routes/user/posts.js
@@ -5,7 +5,7 @@ default Ember.Route.extend({
 
   model: function() {
     var userId = this.modelFor('user').get('id');  // look up modelFor!
-    console.log('posts route', userId);
-    return this.store.find('post', {user: userId});
+    // console.log('posts route', userId);
+    return this.store.find('post', {operation: 'userPosts', user: userId});
   }
 });
