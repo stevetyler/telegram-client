0e7d41c16f92b5b304426ff34d7c79b6249d6064
Steve Tyler <mail@steve-tyler.co.uk>
2015-03-08 20:25:17 +0000

    Repost updates

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -4,12 +4,34 @@ import Ember from 'ember';
 export
 default Ember.ObjectController.extend({
 
-  actions: {
+
+	repostConfirm: false,
+
+	repostLink: function() {
+        return this.get('repostConfirm') ? 'Unrepost' : 'Repost';
+    }.property('repostConfirm'),
+
+	actions: {
     delete: function() {
       this.store.find('post', this.get('model').id).then(function (post) {
         post.deleteRecord();
         post.save();
       });
+    },
+    repost: function() {
+      if (this.get('repostConfirm')) {
+        this.set('repostConfirm', false);   
+      } else {
+        this.set('repostConfirm', true);    
+      } 
+    },
+
+    unrepost: function() {
+      this.set('repostConfirm', false);
     }
-  }
+
+    // yesRepost: function() {
+    //   console.log('yesRepost from the post controller.');
+    // }
+	}
 });
