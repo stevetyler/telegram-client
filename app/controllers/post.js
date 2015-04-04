d755bb4979dc96d0e8dc9737641106ba15459d0a
Steve Tyler <mail@steve-tyler.co.uk>
2015-04-04 22:20:20 +0100

    Moved properties and actions to component

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -5,34 +5,7 @@ export
 default Ember.ObjectController.extend({
 
 
-	repostConfirm: false,
 
-	repostLink: function() {
-        return this.get('repostConfirm') ? 'Unrepost' : 'Repost';
-    }.property('repostConfirm'),
 
-	actions: {
-    // delete: function() {
-    //   this.store.find('post', this.get('model').id).then(function (post) {
-    //     post.deleteRecord();
-    //     post.save();
-    //   });
-    // },
 
-    repost: function() {
-      if (this.get('repostConfirm')) {
-        this.set('repostConfirm', false);
-      } else {
-        this.set('repostConfirm', true);
-      }
-    },
-
-    unrepost: function() {
-      this.set('repostConfirm', false);
-    }
-
-    // yesRepost: function() {
-    //   console.log('yesRepost from the post controller.');
-    // }
-	}
 });
